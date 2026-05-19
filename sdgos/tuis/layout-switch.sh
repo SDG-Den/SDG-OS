f824f6d2bd0e9020a0ad424ff720ff83122528b6
SDGDen <sdgden@sdgcloud.nl>
2026-05-19 18:40:27 +0200

    added layout switcher on super+w

---
+++ sdgos/tuis/layout-switch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mmsg -d togglefloating
+
+
+SELECTED=$(cat ~/.config/sdgos/tuis/layouts.list | cut -d '|' -f 1 | fzf --layout=reverse)
+
+CMD=$(cat ~/.config/sdgos/tuis/layouts.list | grep -e "$SELECTED" | cut -d '|' -f 2)
+
+eval $CMD
